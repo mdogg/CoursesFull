0537ce5fa134361e8b110109b4fe72f53de4bbf8
M Macleod <martincmacleod@hotmail.com>
2017-10-02 14:57:00 +0100

    Label Not working

--- CoursesFull/CoursesIOSFull/CourseFullViewController.cs
+++ CoursesFull/CoursesIOSFull/CourseFullViewController.cs
@@ -14,6 +14,20 @@ namespace CoursesIOSFull
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+            labelTitle.Text = "Tester init";
+            buttonPrev.TouchUpInside += ButtonPrev_TouchUpInside;
+            buttonNext.TouchUpInside += ButtonNext_TouchUpInside;
+        }
+        
+        private void ButtonPrev_TouchUpInside(object sender, EventArgs e)
+        {
+            labelTitle.Text = "Previous Clicked";
+            
+        }
+
+        private void ButtonNext_TouchUpInside(object sender, EventArgs e)
+        {
+            labelTitle.Text = "Next Clicked";
         }
 
         public override void DidReceiveMemoryWarning()
