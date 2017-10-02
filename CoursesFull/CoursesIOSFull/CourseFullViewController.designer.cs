437073cac5c488948b8c6e9eb651b47af295199b
martinmac <martincmacleod@gmail.com>
2017-10-02 12:07:30 +0100

    UI with XCode

--- CoursesFull/CoursesIOSFull/CourseFullViewController.designer.cs
+++ CoursesFull/CoursesIOSFull/CourseFullViewController.designer.cs
@@ -1,21 +1,42 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace CoursesIOSFull
 {
-    [Register("CourseFullViewController")]
-    partial class CourseFullViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("CourseFullViewController")]
+	partial class CourseFullViewController
+	{
+		[Outlet]
+		UIKit.UIButton buttonNext { get; set; }
+
+		[Outlet]
+		UIKit.UIButton buttonPrev { get; set; }
+
+		[Outlet]
+		UIKit.UILabel labelTitle { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (buttonPrev != null) {
+				buttonPrev.Dispose ();
+				buttonPrev = null;
+			}
+
+			if (buttonNext != null) {
+				buttonNext.Dispose ();
+				buttonNext = null;
+			}
+
+			if (labelTitle != null) {
+				labelTitle.Dispose ();
+				labelTitle = null;
+			}
+		}
+	}
 }
