0d0271301e3fa5ece0d20090244608199a7ab1f9
mdogg <martincmacleod@gmail.com>
2017-10-18 16:43:20 +0100

    Moved Label

--- CoursesFull/CoursesIOSFull/CourseFullViewController.designer.cs
+++ CoursesFull/CoursesIOSFull/CourseFullViewController.designer.cs
@@ -1,42 +1,45 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace CoursesIOSFull
 {
-	[Register ("CourseFullViewController")]
-	partial class CourseFullViewController
-	{
-		[Outlet]
-		UIKit.UIButton buttonNext { get; set; }
+    [Register ("CourseFullViewController")]
+    partial class CourseFullViewController
+    {
+        [Outlet]
+        UIKit.UIButton buttonNext { get; set; }
 
-		[Outlet]
-		UIKit.UIButton buttonPrev { get; set; }
 
-		[Outlet]
-		UIKit.UILabel labelTitle { get; set; }
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (buttonPrev != null) {
-				buttonPrev.Dispose ();
-				buttonPrev = null;
-			}
+        [Outlet]
+        UIKit.UIButton buttonPrev { get; set; }
 
-			if (buttonNext != null) {
-				buttonNext.Dispose ();
-				buttonNext = null;
-			}
 
-			if (labelTitle != null) {
-				labelTitle.Dispose ();
-				labelTitle = null;
-			}
-		}
-	}
+        [Outlet]
+        UIKit.UILabel labelTitle { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (buttonNext != null) {
+                buttonNext.Dispose ();
+                buttonNext = null;
+            }
+
+            if (buttonPrev != null) {
+                buttonPrev.Dispose ();
+                buttonPrev = null;
+            }
+
+            if (labelTitle != null) {
+                labelTitle.Dispose ();
+                labelTitle = null;
+            }
+        }
+    }
 }
